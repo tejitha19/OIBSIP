6fa546b0ae8e09e817cb77e14f40b5302fd0d1b5
tejitha19 <tejithamane7@gmail.com>
2024-06-06 21:58:57 +0530

    Add files via upload

---
+++ PASSWORD GENERATOR.py
@@ -0,0 +1,13 @@
+import random
+import string
+length = int(input('Enter the length of password: '))
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+number = string.digits
+symbols = string.punctuation
+
+all = lower + upper + number + symbols
+temp = random.sample(all, length)
+password = "".join(temp)
+print(password)
